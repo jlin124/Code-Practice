101eb2d9c0fb3f223f6c132c99a10f4b876552da
Jennifer Lin <91508494+jlin124@users.noreply.github.com>
2023-08-22 18:12:04 -0700

    Create 1037.cpp

---
+++ beecrowd/1037.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    double num;
+    cin >> num;
+    if((num >= 0.0) && (num <25.01))
+        cout << "Intervalo [0,25]" << endl;
+    else if((num > 25.0) && (num <50.01))
+        cout << "Intervalo (25,50]" << endl;
+    else if((num > 50.0) && (num < 75.01))
+        cout << "Intervalo (50,75]" << endl;
+    else if((num > 75.0) && (num < 100.01))
+        cout << "Intervalo (75,100]" << endl;
+    else
+        cout << "Fora de intervalo" << endl;
+    return 0;
+}
