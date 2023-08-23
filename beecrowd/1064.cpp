683eaba0f04045589c31a03f256f453659b2a224
Jennifer Lin <91508494+jlin124@users.noreply.github.com>
2023-08-22 18:15:58 -0700

    Create 1064.cpp

---
+++ beecrowd/1064.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <iomanip>
+using namespace std;
+
+int main(){
+    double n1, avg;
+    int count;
+    for(int i = 0; i < 6; i++){
+        scanf("%lf", &n1);
+        if(n1 >= 0){
+            count++;
+            avg += n1;
+        }
+    }
+    cout << count << " valores positivos" << endl;
+    cout << fixed << setprecision(1) << avg/count << endl;
+    return 0;
+}
