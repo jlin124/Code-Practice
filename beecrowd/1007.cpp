22369c03708dbb5611cb7489a2fd2716c4360f73
Jennifer Lin <91508494+jlin124@users.noreply.github.com>
2023-08-22 18:06:49 -0700

    Create 1007.cpp

---
+++ beecrowd/1007.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    int ab = a * b;
+    int cd = c * d;
+    cout << "DIFERENCA = " << ab - cd << endl;
+    return 0;
+}
