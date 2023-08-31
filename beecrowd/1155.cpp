98e05c2762adbc11101979c53d3a6adddb561734
Jennifer Lin <91508494+jlin124@users.noreply.github.com>
2023-08-30 18:33:24 -0700

    Create 1155.cpp

---
+++ beecrowd/1155.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+ 
+int main() {
+    double s = 0;
+    for(int i = 1; i <= 100; i++){
+        s += (float)1/i;
+    }
+    cout << fixed << setprecision(2) << s << endl;
+    return 0;
+}
